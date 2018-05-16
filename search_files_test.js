a64459e6507a8412a11011917a7daf7c0afffb8f
Benedikt <benedikt.duerrschmid@gmail.com>
2018-05-16 20:46:46 +0200

    refactoring
    - fixed missing declaration
    - deleted redundant code

--- search_files_test.js
+++ search_files_test.js
@@ -1,157 +1,158 @@
-//command prompt
-var cmd={
-    ask:function(question, callback) {
 
-        var r = rl.createInterface({
-                input: process.stdin,
-                output: process.stdout
+var app=(function(){
+    //command prompt
+    var cmd={
+        ask:function(question, callback) {
+
+            var r = rl.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+
+            r.question(question + '\n', function(answer) {
+
+                r.close();
+                callback(answer);
             });
-
-        r.question(question + '\n', function(answer) {
-
-            r.close();
-            callback(answer);
-        });
-    },
-    progress:function(progress) {
-        process.stdout.clearLine();
-        process.stdout.cursorTo(0);
-        process.stdout.write(progress);
-    }
-}
-
-//read folders recursively
-var readDir=function(folder,done){
-
-    var result=[];
-
-    fs.readdir(folder,function(err,files){
-        
-        var pending=files.length;
-
-        if(!pending){
-            done(null,result);
+        },
+        progress:function(progress) {
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+            process.stdout.write(progress);
         }
-    
-        files.forEach(function(file) {
+    }
 
-            var fileExtension=path.extname(file);
-            var filePath=path.resolve(folder, file);
+    //read folders recursively
+    var readDir=function(folder,done){
 
-            fs.stat(filePath, function(err, stat) {
-    
-                if (stat && stat.isDirectory()) {
+        var result=[];
+
+        fs.readdir(folder,function(err,files){
+            
+            var pending=files.length;
+
+            if(!pending){
+                done(null,result);
+            }
         
-                    readDir(filePath, function(err, response) {
+            files.forEach(function(file) {
+
+                var fileExtension=path.extname(file);
+                var filePath=path.resolve(folder, file);
+
+                fs.stat(filePath, function(err, stat) {
         
-                        result = result.concat(response);
-        
+                    if (stat && stat.isDirectory()) {
+            
+                        readDir(filePath, function(err, response) {
+            
+                            result = result.concat(response);
+            
+                            if (!--pending){
+                                done(null, result);
+                            }
+                        });
+            
+                    } else {
+                        result.push(filePath);
+
                         if (!--pending){
                             done(null, result);
                         }
-                    });
-        
-                } else {
-                    result.push(filePath);
+                    }
+                });
+            });
+        });
+    }
 
-                    if (!--pending){
-                        done(null, result);
+    //read files from folder
+    var readFiles=function(files,term,done){
+
+        var array=[];
+        var pending=files.length;
+
+        files.forEach(function(file){
+            fs.readFile(file, 'utf8', function(err, data) {
+                if(data){
+                    if(data.indexOf(term) >= 0){
+                        array.push("match found in "+file);
                     }
                 }
-            });
-        });
-    });
-}
 
-//read files from folder
-var readFiles=function(files,term,done){
-
-    var array=[];
-    var pending=files.length;
-
-    files.forEach(function(file){
-        fs.readFile(file, 'utf8', function(err, data) {
-            if(data){
-                if(data.indexOf(term) >= 0){
-                    array.push("match found in "+file);
+                if(!--pending){
+                    done(array);
                 }
-            }
-
-            if(!--pending){
-                done(array);
-            }
+            
+            });
         
         });
-    
-    });
-}
+    }
 
-//programm logic
-var run=function(){
+    //programm logic
+    var run=function(){
 
-    console.log("process started");
-    cmd.ask("select dircetory:", function(answer){
+        console.log("process started");
+        cmd.ask("select dircetory:", function(answer){
 
-        var myFolder=answer;
+            var myFolder=answer;
 
-        if(fs.lstatSync(myFolder).isDirectory()){
+            if(fs.lstatSync(myFolder).isDirectory()){
 
-            console.log("start is "+myFolder);
+                console.log("start is "+myFolder);
 
-            cmd.ask("Enter value:", function(answer){
+                cmd.ask("Enter value:", function(answer){
 
-                var myTerm=answer;
+                    var myTerm=answer;
 
-                if(myTerm.length>0){
-                    console.log("searching for \""+myTerm+"\"");
+                    if(myTerm.length>0){
+                        console.log("searching for \""+myTerm+"\"");
 
-                    readDir(myFolder, function(err, result) {
-                    
-                        if(!err){
-                            console.log("searching in "+result.length+" files");
+                        readDir(myFolder, function(err, result) {
+                        
+                            if(!err){
+                                console.log("searching in "+result.length+" files");
 
-                            readFiles(result,myTerm,function(response){
-                                
-                                if(response!=""){
+                                readFiles(result,myTerm,function(response){
+                                    
+                                    if(response!=""){
 
-                                    console.log("found \""+myTerm+"\" in "+response.length+" files");
+                                        console.log("found \""+myTerm+"\" in "+response.length+" files");
 
-                                    cmd.ask("Show results? (Y/N)", function(answer){
-                                        
-                                        switch(answer.toUpperCase()){
-                                            case "Y":
-                                
-                                                response.forEach(function(text){
-                                                    console.log(text);
-                                                });
-                                                
-                                                break;
-                                            default:
-                                                console.log("end");
-                                        }
-                                    });
-                                }else{
-                                    console.log("no files with \""+myTerm+"\" found");
-                                }
-                            })
-                        }else{
-                            console.log(err);
-                        }
-                    });
-                }else{
-                    console.log("no search value defined");
-                }
-            });
-        }else{
-            console.log("cirectory not found");
-        }
-    });
-}
+                                        cmd.ask("Show results? (Y/N)", function(answer){
+                                            
+                                            switch(answer.toUpperCase()){
+                                                case "Y":
+                                    
+                                                    response.forEach(function(text){
+                                                        console.log(text);
+                                                    });
+                                                    
+                                                    break;
+                                                default:
+                                                    console.log("end");
+                                            }
+                                        });
+                                    }else{
+                                        console.log("no files with \""+myTerm+"\" found");
+                                    }
+                                })
+                            }else{
+                                console.log(err);
+                            }
+                        });
+                    }else{
+                        console.log("no search value defined");
+                    }
+                });
+            }else{
+                console.log("cirectory not found");
+            }
+        });
+    }
 
-return {
-    run:run
-}
-
+    return {
+        run:run
+    }
 })();
 
 const fs = require('fs');
